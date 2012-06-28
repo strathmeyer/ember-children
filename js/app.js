ababab6784f22a40cbffb685b28f7655e999c0fe
Eric Strathmeyer <eric@vawks.com>
2012-06-28 15:59:27 -0400

    old working

--- js/app.js
+++ js/app.js
@@ -1,7 +1,32 @@
 var App = Em.Application.create();
 
-App.MyView = Em.View.extend({
-  mouseDown: function() {
-    window.alert("hello world!");
-  }
+App.Person = Em.Object.extend({
+    name: 'John Doe',
+    canHaveChildren: function () {
+        return (this.get('children').length < 3);
+    }.property('children.length')
+});
+
+App.newPerson = function(name) {
+    return App.Person.create({
+        name: name,
+        children: []
+    });
+};
+
+App.rootPerson = App.Person.create({
+    name: 'A',
+    children: []
+});
+
+App.PersonView = Em.View.extend({
+    templateName: 'person',
+    addChild: function() {
+        var children = this.getPath('content.children'),
+            name = prompt('Name:');
+
+        children.unshiftObject(App.newPerson(name));
+
+        console.log(this, children);
+    }
 });
